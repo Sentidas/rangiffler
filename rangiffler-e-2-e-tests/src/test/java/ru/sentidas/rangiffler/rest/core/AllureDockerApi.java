48e0dc3ab12f13b1d8380061f7107b930161ca5b
sentidas <sentidas@yandex.ru>
2025-10-30 12:32:53 +0300

    feat(e2e-tests): add allure docker files and workflow github actions

--- rangiffler-e-2-e-tests/src/test/java/ru/sentidas/rangiffler/rest/core/AllureDockerApi.java
+++ rangiffler-e-2-e-tests/src/test/java/ru/sentidas/rangiffler/rest/core/AllureDockerApi.java
@@ -1,29 +1,29 @@
-//package ru.sentidas.rangiffler.rest.core;
-//
-//import com.fasterxml.jackson.databind.JsonNode;
-//import retrofit2.Call;
-//import retrofit2.http.*;
-//import ru.sentidas.rangiffler.model.allure.AllureProject;
-//import ru.sentidas.rangiffler.model.allure.AllureResults;
-//
-//public interface AllureDockerApi {
-//
-//    @POST("allure-docker-service/send-results")
-//    Call<JsonNode> uploadResults(@Query("project_id") String projectId,
-//                                 @Body AllureResults results);
-//
-//    @GET("allure-docker-service/projects/{project_id}")
-//    Call<JsonNode> project(@Path("project_id") String projectId);
-//
-//    @GET("allure-docker-service/clean-results")
-//    Call<JsonNode> cleanResults(@Query("project_id") String projectId);
-//
-//    @GET("allure-docker-service/generate-report")
-//    Call<JsonNode> generateReport(@Query("project_id") String projectId,
-//                                  @Query("execution_name") String executionName,
-//                                  @Query(value = "execution_from", encoded = true) String executionFrom,
-//                                  @Query("execution_type") String executionType);
-//
-//    @POST("allure-docker-service/projects")
-//    Call<JsonNode> createProject(@Body AllureProject project);
-//}
+package ru.sentidas.rangiffler.rest.core;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import retrofit2.Call;
+import retrofit2.http.*;
+import ru.sentidas.rangiffler.model.allure.AllureProject;
+import ru.sentidas.rangiffler.model.allure.AllureResults;
+
+public interface AllureDockerApi {
+
+    @POST("allure-docker-service/send-results")
+    Call<JsonNode> uploadResults(@Query("project_id") String projectId,
+                                 @Body AllureResults results);
+
+    @GET("allure-docker-service/projects/{project_id}")
+    Call<JsonNode> project(@Path("project_id") String projectId);
+
+    @GET("allure-docker-service/clean-results")
+    Call<JsonNode> cleanResults(@Query("project_id") String projectId);
+
+    @GET("allure-docker-service/generate-report")
+    Call<JsonNode> generateReport(@Query("project_id") String projectId,
+                                  @Query("execution_name") String executionName,
+                                  @Query(value = "execution_from", encoded = true) String executionFrom,
+                                  @Query("execution_type") String executionType);
+
+    @POST("allure-docker-service/projects")
+    Call<JsonNode> createProject(@Body AllureProject project);
+}
